d6fe21778ec36df94afe597b33e5fe1620055d4c
TheAnomaly <sureife@gmail.com>
2017-03-06 11:42:12 +0100

    genesis

---
+++ app/src/main/java/com/alc/alcgit/devdeets/DevDetailActivity.java
@@ -0,0 +1,15 @@
+package com.alc.alcgit.devdeets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alc.alcgit.R;
+
+public class DevDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dev_detail);
+    }
+}
---
+++ app/src/main/java/com/alc/alcgit/devs/DevelopersActivity.java
@@ -0,0 +1,15 @@
+package com.alc.alcgit.devs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alc.alcgit.R;
+
+public class DevelopersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
